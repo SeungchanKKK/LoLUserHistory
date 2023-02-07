f41dcdd3f84fc818ef4b6456ff50f4cdba8c164a
DESKTOP-8FOR6JP\User <eccsck@gmail.com>
2023-02-07 10:59:18 +0900

    [feat] 프로젝트 시작 gradle 빌드및 라이브러리 import
    jpa springweb lombok h2 를 기준으로 프로젝트 빌드

---
+++ src/main/java/com/example/assign/AssignApplication.java
@@ -0,0 +1,13 @@
+package com.example.assign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/assign/AssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
