7ff7287fe2371ac26f7edd4f626d0aa58bfd2d61
seungchanKKK <eccsck@gmail.com>
2023-02-09 19:20:34 +0900

    [refector] 시간 데이터 변경로직 주석 추가

--- src/main/java/com/example/assign/util/TimeCalculator.java
+++ src/main/java/com/example/assign/util/TimeCalculator.java
@@ -7,12 +7,13 @@ import java.util.Date;
 
 @Service
 public class TimeCalculator {
+    //Unix 데이터를 시간및 날짜로 바꿔줍니다
     public  String getTimestampToDate(long timestamp){
         Date date = new Date(timestamp);
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return dateFormat.format(date);
     }
-
+    // 초를 x분 x초 라는 문자열로 바꿔줍니다
     public String getTimefromSec(long sec){
         long minutes = sec / 60;
         long remainingSeconds = sec % 60;
